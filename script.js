2cdcab05c36784488ffe520eb03f828802a38477
Manish Jaunjalkar <117558314+manishjaunjalkar@users.noreply.github.com>
2023-05-02 00:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function removeColor() {
+  const selected = document.getElementById("colorSelect");
+  const selectedInde = selected.selectedIndex;
+  selected.options[selectedInde].remove();
+}
